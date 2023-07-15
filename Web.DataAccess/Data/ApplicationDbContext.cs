acd3578cd952251280cb249376408e8696da4c31
Asif Ahmed <mygithub2305@gmail.com>
2023-07-15 17:18:56 +0200

    added  IdentityUser

--- Web.DataAccess/Data/ApplicationDbContext.cs
+++ Web.DataAccess/Data/ApplicationDbContext.cs
@@ -1,10 +1,11 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Web.Models.Models;
 
 namespace Web.DataAccess.Data
 {
-	public class ApplicationDbContext : IdentityDbContext
+	public class ApplicationDbContext : IdentityDbContext<IdentityUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) :base(options)
         {
