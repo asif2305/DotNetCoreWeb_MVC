893446206d8d6210f03f754c11247dab6a897456
Asif Ahmed <mygithub2305@gmail.com>
2023-07-15 20:34:32 +0200

    complete the course

--- CustomDotNetCoreWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ CustomDotNetCoreWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -107,6 +107,13 @@ namespace CustomDotNetCoreWeb.Areas.Identity.Pages.Account
             public string? Role { get; set; }
             [ValidateNever]
             public IEnumerable<SelectListItem> RoleList { get; set; }
+            [Required]
+            public string Name { get; set; }
+            public string? StreetAddress { get; set; }
+            public string? City { get; set; }
+            public string? State { get; set; }
+            public string? PostalCode { get; set; }
+            public string? PhoneNumber { get; set; }
         }
 
 
@@ -141,7 +148,17 @@ namespace CustomDotNetCoreWeb.Areas.Identity.Pages.Account
 
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
+
+                user.StreetAddress = Input.StreetAddress;
+                user.City = Input.City;
+                user.PostalCode = Input.PostalCode;
+                user.Name= Input.Name; 
+                user.Email=Input.Email;
+                user.PhoneNumber = Input.PhoneNumber;
+                user.State = Input.State;
+
                 var result = await _userManager.CreateAsync(user, Input.Password);
+
 
                 if (result.Succeeded)
                 {
